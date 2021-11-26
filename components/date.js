28d8b78b5232256a8ca5bec418c7c90a648bb0bf
funstra <founstra@gmail.com>
2021-11-26 01:31:56 +0100

    current date

--- components/date.js
+++ components/date.js
@@ -1,33 +1,36 @@
+const curentDate = new Date()
+const yyymmdd = curentDate.toISOString().split('T')[0]
+const maxDate = new Date(curentDate.setDate(curentDate.getDate() + 60))
+const MAXyyymmdd = maxDate.toISOString().split('T')[0]
+console.log(curentDate == maxDate)
+const template = document.createElement('template')
+template.innerHTML = String.raw`
+<style>
+    *{ 
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: monospace;
+    }
+    span{
+        display: inline-block;
+        vertical-align: middle;
+    }
+</style>
+
+<input type="date" value="${yyymmdd}" min="${yyymmdd}" max="${MAXyyymmdd}">
+`
 customElements.define('app-date',
     class appDate extends HTMLElement {
         static get observedAttributes() { return []; }
         constructor() {
             super()
             this.attachShadow({ mode: 'open' })
-            const style = document.createElement('style')
-            this.shadowRoot.appendChild(style)
-            style.textContent = `
-            *{
-                box-sizing: border-box;
-                margin: 0;
-                padding: 0;
-                font-family: monospace;
-            }
-            span{
-                display: inline-block;
-                vertical-align: middle;
-            }
-            `
-            this.shadowRoot.innerHTML += `
-            <input type="date" id="start" name="trip-start"
-                   value="2018-07-22"
-                   min="2018-01-01" max="2018-12-31">
-            
-            `
+            this.shadowRoot.appendChild(template.content.cloneNode(true))
         }
         connectedCallback() { }
         diconnectedCallback() { }
-        attributeChangedCallback(name, oldValue, newValue) { 
-        
+        attributeChangedCallback(name, oldValue, newValue) {
+
         }
     })
