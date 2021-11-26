5c35bff8e609dab5b3fef1d8b21cd72458f69a01
funstra <founstra@gmail.com>
2021-11-26 19:43:11 +0100

    preload SVG

--- www/components/date.js
+++ www/components/date.js
@@ -12,13 +12,52 @@ template.innerHTML = String.raw`
         padding: 0;
         font-family: monospace;
     }
-    span{
-        display: inline-block;
-        vertical-align: middle;
+    :host{
+        display: flex;
     }
+    input[type="date"]{  
+        border: none;
+        background: transparent;
+    }
+    .inner{
+        display: flex;
+    }
+    .date-picker{
+        display:flex;
+        gap: 2ch;
+    }
+    .date-picker div{
+        position: relative;
+        /* background-color: red; */
+    }
+    .date-picker div label{
+        font-size: var(--size-1);
+    }
+    .date-picker div input{
+        position: absolute;
+        opacity: 0;
+    }
+    
 </style>
-
-<input type="date" value="${yyymmdd}" min="${yyymmdd}" max="${MAXyyymmdd}">
+<div class="date-picker">
+    <div>
+        <label for="start-date">
+            <span>d</span>
+            <span>m</span>
+        </label>
+        <input id="start-date" type="date" value="${yyymmdd}" min="${yyymmdd}" max="${MAXyyymmdd}">
+    </div>
+    <div>
+    →
+    </div>
+    <div>
+        <label for="end-date">
+            <span>d</span>
+            <span>m</span>
+        </label>
+        <input id="end-date" type="date" value="${yyymmdd}" min="${yyymmdd}" max="${MAXyyymmdd}">
+    </div>
+</div>
 `
 customElements.define('app-date',
     class appDate extends HTMLElement {
